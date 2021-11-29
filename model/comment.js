19f07728be377838ea251e89c52044be514fe76e
unknown <briankingDCC@gmail.com>
2021-11-29 12:54:46 -0500

    Create replies route in routes folder. Finish comment route

--- model/comment.js
+++ model/comment.js
@@ -1,34 +1,54 @@
 const mongoose = require('mongoose');
-const Joi = require('Joi');
+const joi = require('joi');
 const replySchema = new mongoose.Schema(
     {
+        commentId: {type: String},
         text: { type: String, required: true},
         likes: {type: Number, default: 0},
-        dislikes: {type: Number, default: 0},
+        dislikes: {type: Number, default: 0}
     }
 )
 const commentSchema = new mongoose.Schema(
     {
-        videoId: {type: String, required: true},
         text: { type: String, required: true},
         likes: {type: Number, default: 0},
         dislikes: {type: Number, default: 0},
         replies: {type: [replySchema], default: []}
     }
 )
-/* 
-const Comment = mongoose.model('Comment', commentSchema);
 
 function validateComment(comment) {
-    const schema = Joi.object({
-        videoId: Joi.string().required(),
-        text: Joi.string().required(),
-        likes: Joi.number().default(0),
-        dislikes: Joi.number().default(0),
+    const schema = joi.object({
+        videoId: joi.string().required(),
+        text: joi.string().required(),
+        likes: joi.number().default(0),
+        dislikes: joi.number().default(0),
+
+     
+    });
+    return schema.validate(comment);
+} 
+
+function validateReply(reply) {
+    const schema = joi.object({
+        text: joi.string().required(),
+        likes: joi.number().default(0),
+        dislikes: joi.number().default(0)
      
     })
-} */
+    return schema.validate(reply);
+}
+
 const Reply = mongoose.model('Reply', replySchema);
 const Comment = mongoose.model('Comment', commentSchema);
-module.exports = Reply;
-module.exports = Comment;
+
+// module.exports = validateReply;
+// module.exports = validateComment;
+
+
+exports.Comment = Comment;
+exports.validateComment = validateComment;
+exports.commentSchema = commentSchema;
+exports.Reply = Reply;
+exports.validateReply = validateReply;
+exports.replySchema = replySchema;
--- routes/comments.js
+++ routes/comments.js
@@ -1,22 +1,64 @@
-const comment = require('../model/comment');
+const {Comment, validateComment} = require('../model/comment');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
+    const { error } = validateComment(req.body);
+    if (error)
+       return res.status(400).send(error);
 
-        comment = new Comment({
-            videoId: 'adfadfadf',
-            text: 'hotdogs',
-            likes: 4,
-            dislikes: 3, 
-            replies: 1,
+        const comment = new Comment({
+            videoId: req.body.videoId,
+            text: req.body.text ,
+            likes: req.body.likes,
+            dislikes: req.body.dislikes,
+            replies: req.body.replies
         });
 
         await comment.save();
 
         return res.send(comment);
     } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+   
+    const comment = await Comment.findById(req.params.id);
+    if (!comment)
+    return res.status(400).send(`The product with id "${req.params.Id}" does not exist.`);
+    return res.send(comment);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { error } = validateComment(req.body);
+        if (error) return res.status(400).send(error);
+
+        const comment = await Comment.findByIdAndUpdate(
+            req.params.id,
+            {
+                text: req.body.text,
+                likes: req.body.likes,
+                dislikes: req.body.dislikes,
+                replies: req.body.replies
+            },
+            { new: true }
+        );
+
+        if(!comment)
+            return res.status(400).send(`The comment with id "${req.params.id}" does not exist.`);
+
+            await comment.save();
+
+            return res.send(comment);
+    } catch (ex){
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
